a5f0bb4b55292c33e260810580b547c39cb96cd5
Kjartan Halvorsen <kjartan.halvorsen@gmail.com>
2017-02-21 09:56:42 +0100

    Added notebook for use in computer lab

---
+++ notebooks/non_stationary_skier_process.py
@@ -0,0 +1,34 @@
+def skier_generator_process(env, arrivals):
+    """ Implementation of the process that generates arriving customers to the facility.
+    
+        Arguments
+        env       --  simpy simulation environment object
+        arrivals  --  a list to hold the time of arrivals
+    """
+   
+    k = 0 # A counter to indicate the number of skiers created
+    while True:
+        # Endless loop
+        yield env.timeout( random.expovariate(arrival_freq(env.now)) ) # Wait for a random interval, depending on time of day
+        k += 1
+        #print( "Time is %6.2f and skier %d arrives" % (env.now, k) )
+        arrivals.append(env.now) # Register the time of arrival
+        
+env = simpy.Environment() # Create the simulation environment
+lmbda = 0.5 # One arrival per two minutes on average
+arr = [] # The list of arrival times
+env.process( skier_generator_process(env, lmbda, arr) ) # Tell the simulator to process the skier generator process
+env.run(until=60*8) # Simulate a day
+
+interArrivalTimesAM = np.diff(np.array(arr[:int(len(arr)/2)])) # The interarrival times first half of the day
+interArrivalTimesPM = np.diff(np.array(arr[int(len(arr)/2):])) # The interarrival times second half of the day
+
+plt.figure(figsize=(16,6))
+plt.subplot(121)
+plt.hist(interArrivalTimesAM)
+plt.title('Interarrival times before noon')
+plt.xlim( (0, 15))
+plt.subplot(122)
+plt.hist(interArrivalTimesPM)
+plt.title('Interarrival times afternoon')
+plt.xlim( (0, 15))
